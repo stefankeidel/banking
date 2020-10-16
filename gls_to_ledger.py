032c36db4af1c27b03c7b15c96caa7440431fafd
Stefan <1188614+stefankeidel@users.noreply.github.com>
2020-10-16 17:05:34 +0200

    add hacky script to parse output from GLS bank

---
+++ gls_to_ledger.py
@@ -0,0 +1,74 @@
+# coding: utf-8
+import locale
+import argparse
+import csv
+import pandas as pd
+from datetime import datetime
+
+
+def print_transaction(date, payee, account, category, amount):
+    print("{} {}".format(date.strftime("%Y/%m/%d"), payee))
+    print("    {0:<50}{1:.2f} EUR".format(category, amount))
+    print("    {0}".format(account))
+    print("")
+
+def parse_money(money):
+    return abs(locale.atof(money.replace(".", "")))
+
+def main(gls_filename):
+    with open(gls_filename, encoding='ISO-8859-1') as f:
+        reader = csv.reader(f, delimiter=';')
+        df_l = []
+        for row in reader:
+            if len(row) > 0 and row[0] == 'Buchungstag':
+                df_l.append(row)
+                continue
+
+            if len(df_l) > 0 and len(row) == 0:
+                break
+
+            if len(df_l) > 0:
+                df_l.append(row)
+
+        df = pd.DataFrame(df_l[1:], columns=df_l[0])
+
+    for index, row in df.iterrows():
+        dt = datetime.strptime(row["Valuta"], "%d.%m.%Y")
+
+        # from here we could literally print as is, or
+        # add some business logic to automate certain workflows
+        #
+        # for now, I think we'll do the categories by hand
+
+        if row[' '] == 'S': # Soll == Outflow
+            print_transaction(
+                dt,
+                row['Empfänger/Zahlungspflichtiger'],
+                'Assets:GLS Giro',
+                'Expenses:TODO',
+                parse_money(row['Umsatz'])
+            )
+        elif row[' '] == 'H': # Haben = Income
+            print_transaction(
+                dt,
+                row['Empfänger/Zahlungspflichtiger'],
+                'Income:TODO',
+                'Assets:GLS Giro',
+                parse_money(row['Umsatz'])
+            )
+
+
+if __name__ == "__main__":
+    locale.setlocale(locale.LC_NUMERIC, "de_DE")
+
+    parser = argparse.ArgumentParser(
+        description="Converts GLS Export to ledger output",
+    )
+
+    parser.add_argument(
+        "-f", "--file", type=str, nargs="?", required=True, help="File to parse",
+    )
+
+    args = parser.parse_args()
+
+    main(args.file)
